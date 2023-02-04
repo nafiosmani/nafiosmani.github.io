443a82dda1ce1dcf2c755df27626d946f327924b
nosamni2 <nosmani2@illinois.edu>
2023-02-04 17:50:16 -0600

    promises

---
+++ FESWeek3/app.js
@@ -0,0 +1,14 @@
+// console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
+
+//1. Then
+fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
+    console.log(response.json())
+    response.json().then(data=> {
+        console.log(data)
+    })
+
+})
+
+
+//2. Async/Await
+
