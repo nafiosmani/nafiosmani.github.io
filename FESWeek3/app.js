8056da71c9c21a73c34fb6b84a15d5c0e9220ad6
nosamni2 <nosmani2@illinois.edu>
2023-02-04 18:28:29 -0600

    promises trolling done

--- FESWeek3/app.js
+++ FESWeek3/app.js
@@ -1,14 +1,62 @@
-// console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
+// // console.log(fetch("https://jsonplaceholder.typicode.com/users/1"))
+// const emailRef = document.querySelector(".email");
 
-//1. Then
-fetch("https://jsonplaceholder.typicode.com/users/1").then(response => {
-    console.log(response.json())
-    response.json().then(data=> {
-        console.log(data)
-    })
+// //1. Then
+// // fetch("https://jsonplaceholder.typicode.com/users/1")
+// //   .then((response) => {
+// //     return response.json();
+// //   })
+// //   .then((data) => {
+// //     console.log(data);
+// //   });
 
-})
+// //2. Async/Await
+// async function main() {
+//     const response = await fetch("https://jsonplaceholder.typicode.com/users/1")
+//     const data = await response.json()
+//     console.log(data)
+// }
 
+// main();
+const statusRef = document.querySelector(".status");
+const videoRef = document.querySelector(".video");
 
-//2. Async/Await
+function getSubscriptionStatus() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("undefined");
+    }, 2000);
+  });
+}
 
+// async function main() {
+//   const status = (await getSubscriptionStatus())
+//   statusRef.innerHTML = status;
+// }
+
+// main();
+
+function getVideo(subscriptionStatus) {
+  return new Promise((resolve, reject) => {
+    if (subscriptionStatus === "VIP") {
+      resolve("show video");
+    } else if (subscriptionStatus === "FREE") {
+      resolve("show trailer");
+    } else {
+      reject("no video");
+    }
+  });
+}
+
+async function main() {
+  const status = await getSubscriptionStatus();
+  statusRef.innerHTML = status;
+  try {
+    console.log(await getVideo(status));
+  } catch (e) {
+    console.log(e)
+    videoRef.innerTML = e;
+  }
+}
+
+main();
