d159bd6de6cac2e44dbccffe44cd1909c30ceaa3
nosamni2 <nosmani2@illinois.edu>
2023-02-06 17:11:48 -0600

    beginner

---
+++ FESWeek3/Beginner/app.js
@@ -0,0 +1,31 @@
+function addition(num1, num2) {
+    return num1 + num2
+}
+
+console.log(addition(3,5))
+
+function hrstosecs (hours) {
+    return hours *3600
+}
+
+console.log(hrstosecs(2))
+
+function perimeter(length, width) {
+    return 2*(length + width)
+}
+
+console.log(perimeter(10, 20))
+
+function areatriangle(base, height) {
+    return (1/2)*(base*height)
+}
+
+console.log(areatriangle(10, 10))
+
+function addstring(string1) {
+    return 'Frontend' +string1
+}
+
+console.log(addstring(' Simplified'))
+
+
