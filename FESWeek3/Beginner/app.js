7a41b59260f5121dc8a33f425b29d18932e69ec1
nosamni2 <nosmani2@illinois.edu>
2023-02-06 17:37:20 -0600

    finished beginner

--- FESWeek3/Beginner/app.js
+++ FESWeek3/Beginner/app.js
@@ -29,3 +29,48 @@ function addstring(string1) {
 console.log(addstring(' Simplified'))
 
 
+function greater100(num1, num2) {
+    return (num1 + num2) > 100;
+
+}
+
+console.log(greater100(100, 1))
+
+
+function lessequalzero(num1) {
+    return num1 <= 0;
+}
+
+console.log(lessequalzero(-2))
+
+function opptruth(boolean) {
+    return !boolean;
+}
+
+console.log(opptruth(true))
+
+function notzero(num1) {
+  return num1 !== 0
+}
+
+console.log(notzero(0))
+
+function odd(num1) {
+    return num1 % 2 == 1;
+
+}
+
+console.log(odd(5))
+
+
+function even(num1) {
+    return num1 % 2 === 0 ? 1:-1;
+}
+
+console.log(even(3))
+
+function isLoggAndSub(Log, Sub) {
+    return(Log==='LOG') && (Sub === 'SUB')
+}
+
+console.log(isLoggAndSub('LOG', 'SUB'))
