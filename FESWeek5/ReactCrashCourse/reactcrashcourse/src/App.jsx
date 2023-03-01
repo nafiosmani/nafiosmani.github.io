ce09faa448e66b627663d2cec6426c0e8557b378
nosamni2 <nosmani2@illinois.edu>
2023-03-01 15:58:14 -0600

    start React crash course

---
+++ FESWeek5/ReactCrashCourse/reactcrashcourse/src/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Todo from './components/Todo.jsx';
+import Title from './components/Title.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Title></Title>
+      <Todo></Todo>
+      <Todo></Todo>
+      <Todo></Todo>
+      
+    </div>
+  );
+}
+
+export default App;
