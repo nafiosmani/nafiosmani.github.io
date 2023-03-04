f08cf08b4bd40f81bc54daf896009b92f5c5d934
nosamni2 <nosmani2@illinois.edu>
2023-03-03 18:30:52 -0600

    added prop

--- FESWeek5/ReactCrashCourse/reactcrashcourse/src/App.jsx
+++ FESWeek5/ReactCrashCourse/reactcrashcourse/src/App.jsx
@@ -2,19 +2,29 @@ import logo from './logo.svg';
 import './App.css';
 import Todo from './components/Todo.jsx';
 import Title from './components/Title.jsx';
+import Modal from './components/Modal.jsx';
 
 function App() {
   return (
     <div className="App">
       <Title></Title>
       <div className="todo__wrapper">
-      <Todo></Todo>
-      <Todo></Todo>
-      <Todo></Todo>
+      <Todo title="Finish Frontend Simplified"
+      paragraph= "Code along with Frontend Simplified step by step."
+      />
+
+      <Todo title="Finish Interview Section"
+      paragraph= "Finish every interview question in the next 6 weeks."
+      />
+
+      <Todo title="Land a $100k Job"
+      paragraph= "Apply to 100 jobs."
+      />
       </div>
-      
+      <Modal title="Are you srue want to delete" />
     </div>
   );
 }
 
 export default App;
+
