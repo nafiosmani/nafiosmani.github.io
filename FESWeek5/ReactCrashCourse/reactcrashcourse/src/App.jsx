41bcbc5675d618a29acd3b1daa4722483e8e7103
nosamni2 <nosmani2@illinois.edu>
2023-03-06 15:51:37 -0600

    react hooks

--- FESWeek5/ReactCrashCourse/reactcrashcourse/src/App.jsx
+++ FESWeek5/ReactCrashCourse/reactcrashcourse/src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import Todo from './components/Todo.jsx';
 import Title from './components/Title.jsx';
 import Modal from './components/Modal.jsx';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Counter from './components/Counter.jsx';
 
 
@@ -22,6 +22,18 @@ function App() {
   function confirmModal() {
     setShowModal(false)
   }
+
+  useEffect(() => {
+    console.log('on mount')
+  }, [])
+
+  useEffect(() => {
+    console.log('on mount AND on ${showModal}  change')
+  }, [showModal])
+
+  useEffect(() => {
+    console.log('EVERY render')
+  })
 
   return (
     <div>
