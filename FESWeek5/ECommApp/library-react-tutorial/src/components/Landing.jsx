e6e368d8159229d18e11ec168f7d02aa9b2136bb
nosamni2 <nosmani2@illinois.edu>
2023-03-07 15:18:00 -0600

    landing

--- FESWeek5/ECommApp/library-react-tutorial/src/components/Landing.jsx
+++ FESWeek5/ECommApp/library-react-tutorial/src/components/Landing.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import UndrawBooks from "../assets/undraw_books.svg"
 
-function Landing() {
+const Landing = () => {
   return (
-    <div>Landing</div>
-  )
+    <section id='landing'>
+        <header>
+            <div className="header__container">
+                <div className="header__description"></div>
+                <h1>Australia's most awarded online library platform</h1>
+                <h2>Find your dream book with <span className='purple'>Library</span></h2>
+                <a href="features">
+                    <button className="btn">
+                        Browse books
+                    </button>
+                </a>
+            </div>
+            <figure className='header__img--wrapper'>
+                <img src={UndrawBooks} alt="" />
+            </figure>
+        </header>
+    </section>
+  );
 }
 
-export default Landing
+export default Landing;
