15dbe253455c2d370fbf5099c66773feb513557b
nosamni2 <nosmani2@illinois.edu>
2023-03-08 13:20:15 -0600

    Highlights, Books section

--- FESWeek5/ECommApp/library-react-tutorial/src/App.js
+++ FESWeek5/ECommApp/library-react-tutorial/src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Highlights from './components/Highlights';
-import Nav from './components/Nav';
-import Landing from './components/Landing';
+import Highlights from "./components/Highlights";
+import Nav from "./components/Nav";
+import Landing from "./components/Landing";
+import Featured from "./components/Featured";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Nav />
       <Landing />
       <Highlights />
+      <Featured />
     </div>
   );
 }
