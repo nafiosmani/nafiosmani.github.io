4809fb63cabaa0026485fa143b7982b5cf6f181d
nosamni2 <nosmani2@illinois.edu>
2023-03-08 19:32:36 -0600

    firebase basics

---
+++ FESWeek5/Authentication/firebase-practice/src/App.js
@@ -0,0 +1,66 @@
+import "./App.css";
+import React from "react";
+import { auth } from "./firebase/init";
+import { 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged
+} from "firebase/auth";
+
+function App() {
+  const [user, setUser] = React.useState({});
+  const [loading, setLoading] = React.useState(true);
+
+
+  React.useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setLoading(false);
+      console.log(user.email[0].toUpperCase);
+      if (user) {
+        setUser(user);
+      }
+  })
+  }, []);
+
+
+
+
+  function register() {
+    console.log("register");
+    createUserWithEmailAndPassword(auth, "email@email.com", "test123")
+      .then((user) => {
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  function login() {
+    signInWithEmailAndPassword(auth, 'email@email.com', 'test123')
+    .then(({ user }) => {
+      console.log(user);
+      setUser(user);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
+  function logout() {
+    auth.signOut(auth);
+    setUser({});
+  }
+
+    return (
+      <div className="App">
+        <button onClick={register}>Register</button>
+        <button onClick={login}>Login</button>
+        <button onClick={logout}>Logout</button>
+        {loading ? 'loading...' : user.email}
+      </div>
+    );
+  }
+
+export default App;
---
+++ FESWeek5/Authentication/firebase-practice/src/firebase/init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAvDWwqL6QnjuPC_JDxslfrm6oy2f8qtAg",
+  authDomain: "fir-practice-71e33.firebaseapp.com",
+  projectId: "fir-practice-71e33",
+  storageBucket: "fir-practice-71e33.appspot.com",
+  messagingSenderId: "305574337090",
+  appId: "1:305574337090:web:25848e3b7be5c42d47ebdb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
